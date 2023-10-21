27a3b74c1b883e605e37642116ad77b046576ade
Noaboa97 <noaboa78@gmail.com>
2023-10-22 00:18:15 +0200

    Bugs should be removed, sensor should no correctly detect the moving direction of the object. Second sensor that gets triggered should be the direction the object is moving towards. Will output Links or Rechts in the Console.

--- occupancy-sensor.ino
+++ occupancy-sensor.ino
@@ -1,24 +1,39 @@
 #include <Arduino.h>
 
+// USB-C Port facing towards you for left / right 
+
+// Defining the variables for the pins of the components
 const int irSensorleft = 2;
 const int irSensorright = 32;
 const int ledPinleft = 17;
 const int ledPinright = 26;
 
+// Defining the IR sensor reading variables
+int firstirReadingleft = 0;
 int irReadingleft;
+int firstirReadingright = 0;
 int irReadingright;
 
+// Defining the the variables for time stuff
 unsigned long currentTimeright = 0;
 unsigned long lastReadTimeright = 0;
 
 unsigned long currentTimeleft = 0;
 unsigned long lastReadTimeleft = 0;
 
+unsigned long currentTime = 0;
+
+// Defining the variables for the delay
 unsigned int intervalDelay = 1000;
+unsigned int sensorTriggerDelay = 1000;
+
 
 void setup()
 {
+  // Serial console baud setting
   Serial.begin(9600);
+
+  // PinMode Settings 
   pinMode(irSensorleft, INPUT);
   pinMode(irSensorright, INPUT);
   pinMode(ledPinleft, OUTPUT);
@@ -26,32 +41,45 @@ void setup()
 }
 void loop()
 {
+  // Reading the input of the IR sensor
   irReadingleft = digitalRead(irSensorleft);
   irReadingright = digitalRead(irSensorright);
+
+  // If IR sensor detects motion
   if (irReadingright == LOW)
   {
+    // Turn on the LED
     digitalWrite(ledPinright, HIGH);
+
+    // Capture time of the right IR Sensor when it was triggered
     currentTimeright = millis();
-    // IR Sensor is noisy so we add a debounce mechanism here
+
+    // Loop is so fast so we define a timespan of X for one motion detection
     if (currentTimeright - lastReadTimeright > intervalDelay)
     {
       lastReadTimeright = currentTimeright;
-
+      firstirReadingright = 1;
     }
   }
-  else
+  else // isReadingright == HIGH
   {
+    // Turn off the LED
     digitalWrite(ledPinright, LOW);
   }
+
+  // If IR sensor detects motion
   if (irReadingleft == LOW)
   {
+    // Turn on the LED
     digitalWrite(ledPinleft, HIGH);
+
+    // Capture time of the right IR Sensor when it was triggered
     currentTimeleft = millis();
     // IR Sensor is noisy so we add a debounce mechanism here
     if (currentTimeleft - lastReadTimeleft > intervalDelay)
     {
       lastReadTimeleft = currentTimeleft;
-
+      firstirReadingright = 1;
     }
   }
   else
@@ -59,23 +87,34 @@ void loop()
     digitalWrite(ledPinleft, LOW);
   }
 
-  if(currentTimeleft != 0 && currentTimeright != 0)
+  // get current time in the loop
+  currentTime = millis();
+
+  // Check if it's the first reading 
+  if(firstirReadingleft == 1 || firstirReadingright == 1)
   {
-    if(currentTimeleft > currentTimeright)
+    // Left sensor was triggered last and the left sensor was trigger X ms after the right one
+    if(lastReadTimeleft > lastReadTimeright && lastReadTimeright + sensorTriggerDelay > lastReadTimeleft)
     {
-      // Person ist nach links gelaufen
+      // Person walked left
       Serial.println("Links");
 
     }
-    else if (currentTimeleft < currentTimeright)
+    // Right sensor was triggered last and the right sensor was triggered X ms after the left
+    else if (lastReadTimeleft < lastReadTimeright && lastReadTimeleft + sensorTriggerDelay > lastReadTimeright)
     {
-      // Person ist nach rechts gelaufen
+      // Person walked right
       Serial.println("Rechts");
 
     }
-    currentTimeleft = 0;
-    currentTimeright = 0;
+
   }
+
+  // Used only in the first run of the loop after sensor has detected motion otherwise should be 0 because it's never set to 1 
+  firstirReadingleft = 0;
+  firstirReadingright = 0;
+
+
 
 }
 
